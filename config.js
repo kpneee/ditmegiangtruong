ddd3f6db122c5c9c991947e56f16ebbacae7543d
minquan <91259011+kpneee@users.noreply.github.com>
2022-08-22 20:31:29 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'hế lô chị iu',
+    introDesc: ``,
+    btnIntro: 'típ tục',
+    title: 'chị có yêu em không ',
+    desc: 'không được thoát ra đâu đấy ',
+    btnYes: 'có',
+    btnNo: 'khônggggggggggg',
+    question:'ỏ đáng iu vậy nhg mà sao chưa nói với em',
+    btnReply: 'típ tục',
+    reply: 'tại vì chị ngại á                           ',
+    mess: 'ỏ em cũng thích chị lắm lunnnn',
+    messDesc: 'thế còn chờ gì nữa ib cho e ngay đi <3',
+    btnAccept: 'Accept',
+    messLink: 'https://www.facebook.com/profile.php?id=100037435576667', //link fb
+    bgLink: 'https://bom.to/qjAJcv', //link ảnh nền
+    iputBgLink: 'https://bom.to/CepMcU', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
